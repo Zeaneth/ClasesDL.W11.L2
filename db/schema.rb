591f96b76361468c050c8d2fbc4b52bf18f9e286
Zamiz <zamiztobar@gmail.com>
2018-11-15 20:57:58 -0300

    +Email field to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_231037) do
+ActiveRecord::Schema.define(version: 2018_11_15_235255) do
 
   create_table "students", force: :cascade do |t|
     t.string "names"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
